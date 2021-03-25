1e079061a83c7e6d2e59e1b5e567e76976005e7f
apple <apple@xinghaodeMacBook-Pro-114514.local>
2021-03-25 15:57:20 +0800

    37

--- inceptionv3-for-retinopathy-gpu-hr.py
+++ inceptionv3-for-retinopathy-gpu-hr.py
@@ -365,7 +365,7 @@ def top_2_accuracy(in_gt, in_pred):
 
 ###
 retina_model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Flatten(input_shape=t_x.shape[1:]),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(t_y.shape[-1], activation='softmax')
